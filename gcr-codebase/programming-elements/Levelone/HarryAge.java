a22f39f4979f890f95ad252064639152a78b1fc0
arpit2003-k <arpitgp2003@gmail.com>
2025-12-17 16:18:15 +0530

    [Arpit Gupta] added a programm which Tell the Age of Harry

---
+++ gcr-codebase/programming-elements/Levelone/HarryAge.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+  public class HarryAge {
+
+      public static void main(String args[]) {
+
+        // create the variable 
+        int currentYear=2024;
+        int birthYear=2000;
+
+        //logic for calculating the harry's age 
+        int age = currentYear-birthYear;
+
+        // Resultant The age of harry
+         System.out.println("Harry's age in 2024 is : "+ age); 
+
+
+      }
+	
+  }
