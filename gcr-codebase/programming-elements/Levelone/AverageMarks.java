06eb0106cf97729843f7fa5fbc149604629e0580
arpit2003-k <arpitgp2003@gmail.com>
2025-12-17 16:29:20 +0530

    [Arpit Gupta] Added the program which calculate the average of Sam PCM Marks

---
+++ gcr-codebase/programming-elements/Levelone/AverageMarks.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+  public class AverageMarks {
+
+      public static void main(String args[]) {
+
+        // create the variable 
+        int mathMarks=94;
+        int physicsMarks=95;
+        int chemistryMarks=96;
+
+        //logic for calculating the average
+        int average = (mathMarks+physicsMarks+chemistryMarks)/3;
+
+        // Resultant average marks
+         System.out.println("average Marks of sam's in PCM : "+ average); 
+
+
+      }
+	
+  }
