dcfde88d1e6196dee1f47fd5d3e6a89fbdab8c00
arpit2003-k <arpitgp2003@gmail.com>
2025-12-17 11:57:20 +0530

    [Arpit Gupta] added the Programm of calculating perimeter of Rectangle

---
+++ gcr-codebase/Programming-Pre-requisites/Perimeter.java
@@ -0,0 +1,10 @@
+import.java.util.*;
+class Perimeter{
+	 public static void main(String[] args) {
+        Scanner kb=new Scanner(System.in);
+        
+        int l = kb.nextInt();
+        int b = kb.nextInt();
+        int p = l*b;
+        System.out.println("perimeter of Rectangle is: " + p);
+}
