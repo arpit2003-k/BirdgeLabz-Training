9aaa9350b30aee95d5fb8e17f1ad6baf9ba51c0d
arpit2003-k <arpitgp2003@gmail.com>
2025-12-17 17:01:13 +0530

    [Arpit Gupta] Added The program which calculate the Profit percentage of Item

---
+++ gcr-codebase/programming-elements/Levelone/ProfitPercentage.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+  public class ProfitPercentage {
+
+      public static void main(String args[]) {
+
+        // create the variable 
+        int costPrice=129;
+        int sellingPrice=191;
+
+        //logic for calculating the profit and percentage 
+        int profit = sellingPrice-costprice;
+        int profitPercentage = ((profit*100)/100);
+
+        // Resultant profit with 
+         System.out.println("The cost Price is: "+ costPrice+"The selling price is: "+ sellingPrice);
+         System.out.println("The Profit in INR is: "+ profit +"The profit Percentage is: "+ profitPercentage);
+
+      }
+	
+  }
