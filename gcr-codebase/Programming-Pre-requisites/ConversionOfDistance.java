5fcd9f287c22dbfcb2ce9d58ec86dc1745d048ac
arpit2003-k <arpitgp2003@gmail.com>
2025-12-17 11:58:51 +0530

    [Arpit Gupta] added the Programm of converting the distance of Kilometer into Miles

---
+++ gcr-codebase/Programming-Pre-requisites/ConversionOfDistance.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class ConversionOfDistance {
+    public static void main(String[] args) {
+        Scanner kb=new Scanner(System.in);
+        
+        double k = kb.nextDouble();
+        double m = k*0.621371;
+        System.out.println("Conversion of Distance from kilo meter to miles is : " + m);
+    }
+}
