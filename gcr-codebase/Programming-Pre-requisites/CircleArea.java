354a5ee1316a1873c810d6b4f08b89957913118a
arpit2003-k <arpitgp2003@gmail.com>
2025-12-17 11:56:28 +0530

    [Arpit Gupta] added the Programm of calculating area of circle

---
+++ gcr-codebase/Programming-Pre-requisites/CircleArea.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class CircleArea {
+	public static void main(String args[]){
+		Scanner kb=new Scanner(System.in);
+		double r=kb.nextDouble();
+		System.out.println("Area of circle " + Math.PI*(r*r));
+	
+	}
+}
