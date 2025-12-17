1b381bb764dead0e0893b0ca4a2f2a0a1ff8dd2f
arpit2003-k <arpitgp2003@gmail.com>
2025-12-17 11:53:44 +0530

    [Arpit Gupta] added the Programm of converting C into F

--- gcr-codebase/Programming-Pre-requisites/Conversion.java
+++ gcr-codebase/Programming-Pre-requisites/Conversion.java
@@ -1,9 +1,9 @@
-
+import java.util.*;
 class Conversion {
     public static void main(String[] args) {
-        int rup = 10;
-        int Dollar = 10*85;
-        
-        System.out.println("Rupee into Dollar " + Dollar);
+        Scanner kb=new Scanner(System.in);
+        double c=kb.nextDouble();
+        double f= (10*(9/5))+32;
+        System.out.println("Conversion of Celsius into Fahrenheit" + f);
     }
 }
