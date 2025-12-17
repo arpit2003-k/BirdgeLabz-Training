8e79671b31f72092ffaef7ea3c65635c7e8f368c
arpit2003-k <arpitgp2003@gmail.com>
2025-12-17 16:38:18 +0530

    [Arpit Gupta] Added the convertor which change the KM to Miles

---
+++ gcr-codebase/programming-elements/Levelone/DistanceConvert.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+  public class DistanceConvert {
+
+      public static void main(String args[]) {
+
+        // create the variable 
+        double kilometer=10.8;
+        
+
+        //logic for conversion
+        double miles = (10.8*1.6);
+
+        // Resultant values in miles 
+         System.out.println("Converted value of KM to Miles: "+ miles); 
+
+
+      }
+	
+  }
