6af29a8619e6e4edebd0c7345d7c5f4abd60bc98
arpit2003-k <arpitgp2003@gmail.com>
2025-12-17 11:55:48 +0530

    [Arpit Gupta] added the Programm of calculating average of 3 number

---
+++ gcr-codebase/Programming-Pre-requisites/Average.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Average {
+    public static void main(String[] args) {
+        Scanner kb=new Scanner(System.in);
+        
+        int a = kb.nextInt();
+        int b = kb.nextInt();
+        int c = kb.nextInt();
+        System.out.println("Average of 3 Numbers is : " + (a+b+c)/3);
+    }
+}
