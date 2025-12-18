700f737b72263187c0f7e46043eafaef6533c8bc
arpit2003-k <arpitgp2003@gmail.com>
2025-12-18 12:53:10 +0530

    [Arpit gupta] Added the program which perform the arthmetic operation on Double data type

--- gcr-codebase/programming-elements/LevelTwo/DoubleOperation.java
+++ gcr-codebase/programming-elements/LevelTwo/DoubleOperation.java
@@ -18,11 +18,12 @@ public class DoubleOperation {
 		double op3 = c+(a/b);
 		double op4 = (a%b)+c;
 
+
 		System.out.println("The results of Double Operations are, " + op1 + " , " + op2 + " , " + op3 + " and " + op4);
 	
 	}
 	public static void main(String [] args){
 		solveExpression();
 	}
-	
+
  }
