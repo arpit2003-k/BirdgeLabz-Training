dac012f62816d0e92e4265dd3b540e710f54c192
arpit2003-k <arpitgp2003@gmail.com>
2025-12-18 12:51:15 +0530

    [Arpit gupta] Added the program to convert the feet to yard and yard to miles

--- gcr-codebase/programming-elements/LevelTwo/ConversionYardToMiles.java
+++ gcr-codebase/programming-elements/LevelTwo/ConversionYardToMiles.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class ConversionYardToMiles {
     public static void main(String[] args) {
+        
         Scanner input = new Scanner(System.in);
 
         double feet = input.nextDouble();
