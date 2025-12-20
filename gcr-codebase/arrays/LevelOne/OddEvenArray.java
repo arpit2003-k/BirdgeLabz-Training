6b370707514d2fac498db60370f0578f40bf7469
arpit2003-k <arpitgp2003@gmail.com>
2025-12-20 12:14:37 +0530

    [Arpit Gupta ]Added the program the for Display the Odd and even from the 1 to the given Number

---
+++ gcr-codebase/arrays/LevelOne/OddEvenArray.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class OddEvenArray {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        if (number <= 0) {
+            System.err.println("Error: Please enter a natural number.");
+            System.exit(0); // exit program
+        }
+
+        int size = number / 2 + 1;
+        int[] even = new int[size];
+        int[] odd = new int[size];
+
+        int evenIndex = 0;
+        int oddIndex = 0;
+
+        for (int i = 1; i <= number; i++) {
+            if (i % 2 == 0) {
+                even[evenIndex] = i;
+                evenIndex++;
+            } else {
+                odd[oddIndex] = i;
+                oddIndex++;
+            }
+        }
+
+        System.out.println("\nOdd Numbers:");
+        for (int i = 0; i < oddIndex; i++) {
+            System.out.print(odd[i] + " ");
+        }
+
+        System.out.println("\n\nEven Numbers:");
+        for (int i = 0; i < evenIndex; i++) {
+            System.out.print(even[i] + " ");
+        }
+    }
+}
