d76c2b008c0e34ebb2ebcda62045ab7b70143200
arpit2003-k <arpitgp2003@gmail.com>
2025-12-21 00:21:17 +0530

    [Arpit Gupta]Added a program to find the multiple of the given number  using while loop

---
+++ gcr-codebase/control-flow/LevelTwo/MultiplesBelowHundredWhile.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class MultiplesBelowHundredWhile {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Take input
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        // Validate input
+        if (number <= 0 || number >= 100) {
+            System.out.println("Error: Please enter a positive integer less than 100.");
+            return;
+        }
+
+        //  Loop backward from 100 to 1
+        System.out.println(" Multiples of " + number + " below 100:");
+        int i = 100;
+
+        while ( i >= 1) {
+
+            //  Check if i is a multiple of number
+            if (i % number == 0) {
+                System.out.println(i);
+            }
+            i--;
+        }
+    }
+}
