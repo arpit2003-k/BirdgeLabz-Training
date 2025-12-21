122ea3a4e6a319de25da6da5d969f0a1d7714073
arpit2003-k <arpitgp2003@gmail.com>
2025-12-22 01:41:45 +0530

    [Arpit Gupta]Added the program to find the grades of the student storing it into the 2D array

---
+++ gcr-codebase/arrays/LevelTwo/StudentGrades2DArray.java
@@ -0,0 +1,48 @@
+/*Rewrite the above program to store the marks of the students in physics, chemistry, and maths in a 2D array and then compute the percentage and grade
+Hint => 
+All the steps are the same as the problem 8 except the marks are stored in a 2D array
+Use the 2D array to calculate the percentages, and grades of the students
+*/
+
+//using 2D array
+
+import java.util.Scanner;
+
+public class StudentGrades2DArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+
+        int[][] marks = new int[n][3];
+        double[] percentage = new double[n];
+        char[] grade = new char[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Physics: ");
+            marks[i][0] = sc.nextInt();
+
+            System.out.print("Chemistry: ");
+            marks[i][1] = sc.nextInt();
+
+            System.out.print("Maths: ");
+            marks[i][2] = sc.nextInt();
+
+            percentage[i] = (marks[i][0] + marks[i][1] + marks[i][2]) / 3.0;
+
+            if (percentage[i] >= 80) grade[i] = 'A';
+            else if (percentage[i] >= 70) grade[i] = 'B';
+            else if (percentage[i] >= 60) grade[i] = 'C';
+            else if (percentage[i] >= 50) grade[i] = 'D';
+            else if (percentage[i] >= 40) grade[i] = 'E';
+            else grade[i] = 'R';
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Student " + (i + 1));
+            System.out.println("Percentage: " + percentage[i]);
+            System.out.println("Grade: " + grade[i]);
+        }
+    }
+}
