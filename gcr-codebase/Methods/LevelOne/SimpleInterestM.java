bb6168ea4e3d759963fe733f7a834028f4a7d115
arpit2003-k <arpitgp2003@gmail.com>
2025-12-22 12:13:33 +0530

    [Arpit Gupta]Added the program to calculate SI from P R T  using method

---
+++ gcr-codebase/Methods/LevelOne/SimpleInterestM.java
@@ -0,0 +1,36 @@
+/*Write a program to input the Principal, Rate, and Time values and calculate Simple Interest.
+Hint => 
+Simple Interest = Principal * Rate * Time / 100
+Take user input for principal, rate, time
+Write a method to calculate the simple interest given principle, rate and time as parameters
+Output “The Simple Interest is ___ for Principal ___, Rate of Interest ___ and Time ___”
+*/
+
+import java.util.Scanner;
+
+public class SimpleInterestM {
+    
+    static double calculateSI(double principal, double rate, double time) {
+        return (principal * rate * time) / 100;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Principal: ");
+        double principal = sc.nextDouble();
+
+        System.out.print("Enter Rate of Interest: ");
+        double rate = sc.nextDouble();
+
+        System.out.print("Enter Time: ");
+        double time = sc.nextDouble();
+
+        double si = calculateSI(principal, rate, time);
+
+        System.out.println("The Simple Interest is " + si +
+                " for Principal " + principal +
+                ", Rate of Interest " + rate +
+                " and Time " + time);
+    }
+}
