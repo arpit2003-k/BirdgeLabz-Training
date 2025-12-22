c9b3d5a90e6f572429d6893dfdf7380915681a78
arpit2003-k <arpitgp2003@gmail.com>
2025-12-22 12:32:15 +0530

    [Arpit Gupta]Added the program to DO the unit conversion for more unit like weight temprature and length  by making Static utility method for each conversion

---
+++ gcr-codebase/Methods/LevelTwo/UnitConvertorExtendedStatic.java
@@ -0,0 +1,70 @@
+/*Extend or Create a UnitConvertor utility class similar to the one shown in the notes to do the following.  Please define static methods for all the UnitConvertor class methods. E.g. 
+public static double convertFarhenheitToCelsius(double farhenheit) => 
+Method to convert Fahrenheit to Celsius and return the value. Use the following code  double farhenheit2celsius = (farhenheit - 32) * 5 / 9;
+Method to convert Celsius to Fahrenheit and return the value. Use the following code  double celsius2farhenheit = (celsius * 9 / 5) + 32;
+Method to convert pounds to kilograms and return the value. Use the following code  double pounds2kilograms = 0.453592;
+Method to convert kilograms to pounds and return the value. Use the following code  double kilograms2pounds = 2.20462; 
+Method to convert gallons to liters and return the value. Use following code to convert   double gallons2liters = 3.78541; 
+Method to convert liters to gallons and return the value. Use following code to convert  double liters2gallons = 0.264172; 
+*/
+
+import java.util.Scanner;
+
+public class UnitConvertorExtendedStatic {
+
+    public static double convertFarhenheitToCelsius(double farhenheit) {
+        return (farhenheit - 32) * 5 / 9;
+    }
+
+    public static double convertCelsiusToFarhenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    public static double convertPoundsToKilograms(double pounds) {
+        double pounds2kilograms = 0.453592;
+        return pounds * pounds2kilograms;
+    }
+
+    public static double convertKilogramsToPounds(double kilograms) {
+        double kilograms2pounds = 2.20462;
+        return kilograms * kilograms2pounds;
+    }
+
+    public static double convertGallonsToLiters(double gallons) {
+        double gallons2liters = 3.78541;
+        return gallons * gallons2liters;
+    }
+
+    public static double convertLitersToGallons(double liters) {
+        double liters2gallons = 0.264172;
+        return liters * liters2gallons;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter temperature in Fahrenheit: ");
+        double f = sc.nextDouble();
+        System.out.println("Celsius = " + convertFarhenheitToCelsius(f));
+
+        System.out.print("Enter temperature in Celsius: ");
+        double c = sc.nextDouble();
+        System.out.println("Fahrenheit = " + convertCelsiusToFarhenheit(c));
+
+        System.out.print("Enter weight in pounds: ");
+        double pounds = sc.nextDouble();
+        System.out.println("Kilograms = " + convertPoundsToKilograms(pounds));
+
+        System.out.print("Enter weight in kilograms: ");
+        double kg = sc.nextDouble();
+        System.out.println("Pounds = " + convertKilogramsToPounds(kg));
+
+        System.out.print("Enter gallons: ");
+        double gallons = sc.nextDouble();
+        System.out.println("Liters = " + convertGallonsToLiters(gallons));
+
+        System.out.print("Enter liters: ");
+        double liters = sc.nextDouble();
+        System.out.println("Gallons = " + convertLitersToGallons(liters));
+    }
+}
