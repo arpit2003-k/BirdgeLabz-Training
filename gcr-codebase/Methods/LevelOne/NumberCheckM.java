7ad3edaa657744f18a4066974d54c50a3bcd5ce4
arpit2003-k <arpitgp2003@gmail.com>
2025-12-22 12:11:39 +0530

    [Arpit Gupta]Added the program to find what kind of number it is using method

---
+++ gcr-codebase/Methods/LevelOne/NumberCheckM.java
@@ -0,0 +1,33 @@
+/*Write a program to check whether a number is positive, negative, or zero.
+Hint => Get integer input from the user. Write a Method to return -1 for negative number, 1 for positive number and 0 if number is zero
+*/
+
+import java.util.Scanner;
+
+public class NumberCheckM {
+
+    static int checkNumber(int num) {
+        if (num > 0)
+            return 1;
+        else if (num < 0)
+            return -1;
+        else
+            return 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int result = checkNumber(num);
+
+        if (result == 1)
+            System.out.println("Positive Number");
+        else if (result == -1)
+            System.out.println("Negative Number");
+        else
+            System.out.println("Zero");
+    }
+}
