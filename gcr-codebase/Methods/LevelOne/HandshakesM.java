dadfd9f7a3aa9fef91e11036dfebb13bb5965af3
arpit2003-k <arpitgp2003@gmail.com>
2025-12-22 12:10:47 +0530

    [Arpit Gupta]Added the program to find the NUmber of handshakes using the method

---
+++ gcr-codebase/Methods/LevelOne/HandshakesM.java
@@ -0,0 +1,27 @@
+/*Create a program to find the maximum number of handshakes among students.
+Hint => 
+Get integer input for the numberOfStudents variable.
+Use the combination = (n * (n - 1)) / 2 formula to calculate the maximum number of possible handshakes.
+Write a method to use the combination formulae to calculate the number of handshakes
+Display the number of possible handshakes.
+*/
+
+import java.util.Scanner;
+
+public class HandshakesM {
+
+    static int calculateHandshakes(int n) {
+        return (n * (n - 1)) / 2;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of students: ");
+        int numberOfStudents = sc.nextInt();
+
+        int handshakes = calculateHandshakes(numberOfStudents);
+
+        System.out.println("Maximum number of handshakes = " + handshakes);
+    }
+}
