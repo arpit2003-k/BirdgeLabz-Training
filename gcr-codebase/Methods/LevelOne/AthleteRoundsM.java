ed958c9d9b8a821222a4293df39d6bde6bf43b2b
arpit2003-k <arpitgp2003@gmail.com>
2025-12-22 12:07:39 +0530

    [Arpit Gupta]Added the program to find the number of rounds to add that a athelete will run in the triangular park

---
+++ gcr-codebase/Methods/LevelOne/AthleteRoundsM.java
@@ -0,0 +1,30 @@
+/*An athlete runs in a triangular park with sides provided as input by the user in meters. If the athlete wants to complete a 5 km run, then how many rounds must the athlete complete
+Hint => 
+Take user input for 3 sides of a triangle 
+The perimeter of a triangle is the addition of all sides and rounds is distance/perimeter
+Write a Method to compute the number of rounds user needs to do to complete 5km run
+*/
+
+import java.util.Scanner;
+
+public class AthleteRoundsM {
+
+    static double calculateRounds(double a, double b, double c) {
+        double perimeter = a + b + c;
+        return 5000 / perimeter;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter side 1: ");
+        double a = sc.nextDouble();
+        System.out.print("Enter side 2: ");
+        double b = sc.nextDouble();
+        System.out.print("Enter side 3: ");
+        double c = sc.nextDouble();
+
+        double rounds = calculateRounds(a, b, c);
+        System.out.println("Rounds required to complete 5km = " + rounds);
+    }
+}
