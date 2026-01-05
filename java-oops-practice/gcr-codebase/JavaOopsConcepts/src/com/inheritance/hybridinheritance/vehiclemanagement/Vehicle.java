114806890d42467892220085434457a4d70ce089
arpit2003-k <arpitgp2003@gmail.com>
2026-01-05 16:58:51 +0530

    [Arpit Gupta]: Added the program that shows the Vehicle  management system  using the concept of hybrid  inheritance

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hybridinheritance/vehiclemanagement/Vehicle.java
@@ -0,0 +1,11 @@
+package com.inheritance.hybridinheritance.vehiclemanagement;
+
+public class Vehicle {
+	int maxSpeed;
+    String model;
+
+    Vehicle(int maxSpeed, String model) {
+        this.maxSpeed = maxSpeed;
+        this.model = model;
+    }
+}
