c5d78f473a681dd5810df9d52cd98653203983c5
arpit2003-k <arpitgp2003@gmail.com>
2026-01-20 16:16:30 +0530

    [Arpit gupta]:Added the program for the Implementing the Selection sort Algorithm

---
+++ java-dsa-practice/gcr-codebase/JavaDsaPractice/src/com/sortingalgorithm/selectionsort/SelectionSort.java
@@ -0,0 +1,38 @@
+package com.sortingalgorithm.selectionsort;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+	public static void selectionSort(int[] scores) {
+
+        int n = scores.length;
+
+        for (int i = 0; i < n - 1; i++) {
+
+            int minIndex = i;
+
+            // Find minimum element in unsorted part
+            for (int j = i + 1; j < n; j++) {
+                if (scores[j] < scores[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap
+            int temp = scores[minIndex];
+            scores[minIndex] = scores[i];
+            scores[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] examScores = { 78, 92, 65, 88, 70 };
+
+        System.out.println("Before Sorting: " + Arrays.toString(examScores));
+
+        selectionSort(examScores);
+
+        System.out.println("After Sorting:  " + Arrays.toString(examScores));
+    }
+}
