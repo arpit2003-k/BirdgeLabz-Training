c10c45665ab9dfee91644bff4c8545fe056c53cd
arpit2003-k <arpitgp2003@gmail.com>
2026-01-21 15:03:52 +0530

    [Arpit gupta]:Added the program for the Remove duplicate  string using the StringBuilder

---
+++ java-dsa-practice/gcr-codebase/JavaDsaPractice/src/com/stringbuilder/RemoveDuplicates.java
@@ -0,0 +1,22 @@
+package com.stringbuilder;
+import java.util.HashSet;
+
+public class RemoveDuplicates {
+
+    public static String removeDuplicates(String str) {
+        StringBuilder sb = new StringBuilder();
+        HashSet<Character> set = new HashSet<>();
+
+        for (char c : str.toCharArray()) {
+            if (!set.contains(c)) {
+                sb.append(c);
+                set.add(c);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(removeDuplicates("programming"));
+    }
+}
