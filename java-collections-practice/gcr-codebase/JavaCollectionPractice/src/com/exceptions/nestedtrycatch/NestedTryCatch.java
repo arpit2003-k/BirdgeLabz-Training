47cabc01357d57fa3b5c32eccc24473d1b26f6ab
arpit2003-k <arpitgp2003@gmail.com>
2026-02-02 14:43:41 +0530

    [Arpit Gupta]:Added the Program for handling the exception by using nested try catch block using the concept of Exception handling

---
+++ java-collections-practice/gcr-codebase/JavaCollectionPractice/src/com/exceptions/nestedtrycatch/NestedTryCatch.java
@@ -0,0 +1,21 @@
+package com.exceptions.nestedtrycatch;
+
+public class NestedTryCatch {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30};
+        int index = 2;
+        int divisor = 0;
+
+        try {
+            try {
+                int result = arr[index] / divisor;
+                System.out.println("Result: " + result);
+            } catch (ArithmeticException e) {
+                System.out.println("Cannot divide by zero!");
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid array index!");
+        }
+    }
+}
