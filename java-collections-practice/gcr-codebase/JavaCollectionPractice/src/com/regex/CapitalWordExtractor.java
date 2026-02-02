c798aa373e9688a71c88dfb4d28c48a1054ecc97
arpit2003-k <arpitgp2003@gmail.com>
2026-02-02 15:17:57 +0530

    [Arpit Gupta]:Added the program for the Identifing the Capital word and printing it using the Regex Expression in java

---
+++ java-collections-practice/gcr-codebase/JavaCollectionPractice/src/com/regex/CapitalWordExtractor.java
@@ -0,0 +1,16 @@
+package com.regex;
+import java.util.regex.*;
+
+public class CapitalWordExtractor {
+    public static void main(String[] args) {
+        String text = "The Eiffel Tower is in Paris and New York";
+
+        String regex = "\\b[A-Z][a-z]*\\b";
+
+        Matcher matcher = Pattern.compile(regex).matcher(text);
+
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+    }
+}
