3a06232183fd4e30f7091b2fe7d958b069ab242d
arpit2003-k <arpitgp2003@gmail.com>
2026-02-16 14:29:18 +0530

    [Arpit Gupta]: Added program to perform custom sorting in e-commerce products using lambda expressions and Comparator in Java 8

---
+++ java-eight-practice/gcr-codebase/JavaEightPractice/src/com/lambdaexpression/customsortinfecommerce/Product.java
@@ -0,0 +1,24 @@
+package com.lambdaexpression.customsortinfecommerce;
+
+class Product {
+    String name;
+    double price;
+    double rating;
+    double discount;
+
+    Product(String name, double price, double rating, double discount) {
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+        this.discount = discount;
+    }
+    
+    
+
+    @Override
+    public String toString() {
+        return "   "+ name + " | Price: " + price +
+               " | Rating: " + rating +
+               " | Discount: " + discount;
+    }
+}
